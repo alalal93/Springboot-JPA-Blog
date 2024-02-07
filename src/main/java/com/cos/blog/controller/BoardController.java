2ef76e3b739fcb55bef32190f4eaf8cd3abaa193
alalal93 <alalal9310@gmail.com>
2024-02-07 20:46:44 +0900

    게시판중간저장

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.blog.config.auth.PrincipalDetail;
 
-@Controller
-public class BoardController {
+	@Controller
+	public class BoardController {
 	
-	
-	
-@GetMapping({"","/"})
+		@GetMapping({ "", "/" })
 	public String index() { // 컨트롤러에서 세션을 어떻게 찾는지?
-	// /WEB-INF/views/index.jsp
-
+		// /WEB-INF/views/index.jsp
+	
 		return "index";
 	}
+	// USER 권한이 필요
+		@GetMapping("board/saveForm")
+		public String saveForm() {
+			return "board/saveForm";
+		}
 }
